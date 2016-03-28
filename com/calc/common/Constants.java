dc0767256742df4e03c8b8c20abd608d47c87045
kowsalyajaganathan <kowsalyajaganathan@gmail.com>
2016-03-28 01:57:47 -0600

    Added files via upload

---
+++ com/calc/common/Constants.java
@@ -0,0 +1,30 @@
+package com.calc.common;
+
+
+/**
+ *Constants.java
+ *
+ *Version:1.0
+ *Date: 24-Mar-2016
+ *Author:Kowsalya Jaganathan
+ *
+ *This class is to hold the constant values to be used through out the application
+ *
+*/
+
+public class Constants{
+	public static final String ADDITION="add";
+	public static final String SUBTRACTION="sub";
+	public static final String MULTIPLICATION="mult";
+	public static final String DIVISION="div";
+	public static final String ASSIGNMENT="let";
+	
+	public static final String LEFT="L";
+	public static final String RIGHT="R";
+
+	public static final String MSG_INVALID_OPERATOR="Invalid operator in the expression";
+	public static final String MSG_PARSE_ERROR="Input expression parsing ended prematurely ";
+	public static final String MSG_EXECUTE_ERROR="Expression cannot  be evaluated";
+	public static final String MSG_INVALID_INPUT="Invaid expression";
+	
+}
